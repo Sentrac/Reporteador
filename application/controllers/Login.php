86450bfe3491259cbe1b0221c6f4e39349a0b868
ROsa <rosa@warriors.gateway.huawei.net>
2018-12-08 15:12:12 -0600

    agregando funciones de MC al login 08/12/18

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -12,10 +12,34 @@ class Login extends CI_Controller {
 		$this->load->library('session');
 	}
 
-	public function index()
-	{
+	public function index(){
         $this->load->view('temps/header');         
 		$this->load->view('interfaces/index');
 		$this->load->view('temps/footer');         
+	}
+
+	public function proceso_login(){
+		$email=$this->input->post('email');
+		$pass=$this->input->post('pass');
+		
+		$checklogin=$this->Modelo_login->login($email,$pass);
+
+        if($checklogin){
+            foreach ($checklogin as $row);
+                $this->session->set_userdata('email',$row->email);
+                $this->session->set_userdata('tipo_usuario',$row->tipo_usuario);
+                //USUARIO SUPERUSUARIO
+                if($this->session->userdata('tipo_usuario')=="SU"){
+                    redirect('superusuario/index');
+                }
+                //USUARIO ADMINISTRADOR
+                elseif($this->session->userdata('tipo_usuario')=="AD"){
+                    redirect('admin/index');
+                }
+                //USUARIO CONSULTAS
+                elseif($this->session->userdata('tipo_usuario')=='CO'){
+                    redirect('consultas/index');
+                }
+		}
 	}
 }
--- application/models/modelo_login.php
+++ application/models/modelo_login.php
@@ -2,8 +2,19 @@
 
 class Modelo_login extends CI_Model{
 
-    function login(){
-        
+    function login($mat,$pass){
+        /*FUNCIÓN PARA CONSULTAR LOS CAMPOS DE LA TABLA 'usuarios'*/
+        $this->db->select('email, pass, tipo_usuario');
+        $this->db->from('usuarios');
+        $this->db->where('email',$email);
+        $this->db->where('pass',$pass);
+        $this->db->limit(1);
+        $query=$this->db->get();
+        if($query->num_rows()==1){
+            return $query->result();
+        }else {
+            return false;
+        }
     }
 }
 ?>
