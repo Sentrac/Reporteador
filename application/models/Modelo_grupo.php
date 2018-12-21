861d62313150f719f4eb3700abcca89592695faa
ROsa <enriqRosa>
2018-12-21 13:11:16 -0600

    funcion y validación de formulario agregar grupos 21/12/18

--- application/models/Modelo_grupo.php
+++ application/models/Modelo_grupo.php
@@ -2,12 +2,21 @@
 
 class Modelo_grupo extends CI_Model{
 
+    //FUNCION DONDE SE MUESTRAN TODOS LOS GRUPOS MENOS EL DE ID=1
     public function grupos_lista(){
         $this->db->distinct('nombre');
         $this->db->where_not_in('idgrupo', 1);
         $data=$this->db->get('grupo');
         return $data->result();
     }
-
+    //FUNCIÓN PARA INSERTAR EN LA TABLA GRUPO 
+    public function registrarGrupos($array){
+        $count=$this->db->insert('grupo',$array);        
+        if($count>0){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
 ?>
