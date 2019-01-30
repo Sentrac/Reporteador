2b9fa860c4ea28f1357e40ac9806ed15dc298ff4
ROsa <enriqRosa>
2019-01-30 10:00:16 -0600

    Mofificación FINAL ruta nginx

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://189.204.31.154:81/';
-http://wreporter.warriorslabs.com:81/
+$config['base_url'] = 'http://189.204.31.154:81/Reporteador/';
 /*
 |--------------------------------------------------------------------------
 | Index File
