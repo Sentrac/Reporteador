9ccfbed589bf7bb6d1101691ce39df8dc6cab315
Sentrac <josue.marin1005@gmail.com>
2019-03-06 15:15:32 -0600

    modificacion de la base url 06-03-19

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://wreporter.warriorslabs.com:82/Reporteador/';
+$config['base_url'] = 'http://189.204.31.154:82/Reporteador/';
 /*
 |--------------------------------------------------------------------------
 | Index File
